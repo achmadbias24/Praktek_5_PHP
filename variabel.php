28ae2cdf2924e150e7979270bb7d31cf4f2f1705
achmadbias24 <79568791+achmadbias24@users.noreply.github.com>
2021-03-28 10:55:27 +0700

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variabel</title>
+</head>
+<body>
+	<!--deklarasi script PHP-->
+	<?php
+		//deklarasi variabel
+		$nilai_1=10;
+		$nilai_2=3;
+		$nilai_3=2*$nilai_1+8*$nilai_2;
+		//menampilkan nilai variabel
+		echo "nilai = ",$nilai_3;
+		echo "<br>";
+		//deklarasi variabel yang berisi operasi aritmatika
+		$jumlah=$nilai_1+$nilai_2;
+		//menampilkan nilai variabel
+		echo "hasil dari $nilai_1 + $nilai_2 adalah : $jumlah";echo "<br>";
+		//menuliskan kalimat secara langsung
+		echo "\"Nama : Achmad Bias Firmansyah\"<br>";
+		echo "NPM : 19082010033";
+	?>
+</body>
+</html>
